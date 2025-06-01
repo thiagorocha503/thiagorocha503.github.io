d68cb37ca29668b85b15d04d1187f9d21135b30b
Thiago Ferreira <thiagorocha503@gmail.com>
2025-06-01 11:23:10 -0300

    feat: remove contact email

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,9 +1,7 @@
 export const CONTACT_FORM_ACTION = "https://submit-form.com/5qnUN5hc";
-export const CONTACT_EMAIL = "contact@thiagorochaferreira.com.br";
 export const GITHUB_URL = "https://github.com/thiagorocha503";
 export const INSTAGRAM_URL = "https://www.instagram.com/thiago_rocha503/";
 export const FACEBOOK_URL = "http://www.facebook.com/profile.php?id=100007633775339";
-
 export const CARD_MEMORY_GAME_IMAGE_URL = "https://i.ibb.co/RQR2y7Q/card memory game.png";
 export const MARKDOWN_PREVIEWER_IMAGE_URL = "https://i.ibb.co/fHyz9wy/markdown previewer.png";
 export const BOOK_FINDER_IMAGE_URL = "https://i.ibb.co/2dHH26p/book-finder.png";
